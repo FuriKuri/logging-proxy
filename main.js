ddf00567eb3916c2f989a561b0f7d96b46cda4e7
Theo Pack <tf.pack@gmail.com>
2018-08-10 09:27:31 +0200

    Fix ports

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ process.on("uncaughtException", function(error) {
 });
 
 var localport = 8000;
-var remotehost = '172.17.0.1';
-var remoteport = 8080;
+var remotehost = '142.93.170.117';
+var remoteport = 80;
 
 var server = net.createServer(function (localsocket) {
   var remotesocket = new net.Socket();
